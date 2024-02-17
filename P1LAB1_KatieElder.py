450d70794c694a315378f5bb0aa107ac5bcf1f8f
CtrlKatieAltDel <160362428+CtrlKatieAltDel@users.noreply.github.com>
2024-02-17 17:06:16 -0500

    First submission
    First submission

---
+++ P1LAB1_KatieElder.py
@@ -0,0 +1,7 @@
+# Katie Elder
+# 17Feb2024
+# P1LAB1
+# This program takes a first name as input and outputs a welcome message to that name.
+
+user_name = input("Enter your name: ")
+print("Hello " + user_name + " and welcome to zyBooks!")
